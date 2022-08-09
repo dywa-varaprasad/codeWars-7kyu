f95811f0b73b53e72d7db3ad364a0426bee3ac6e
DywaVaraprasad <105714346+dywa-varaprasad@users.noreply.github.com>
2022-08-09 14:04:19 +0530

    Create isPalindromeChecker.js

---
+++ isPalindromeChecker.js
@@ -0,0 +1,7 @@
+//create a function to check a word a palindrome or not!
+
+const isPalindromeChecker(string) =>{
+let reverseString = str.split('').reverse().join('')
+return reverseString =string
+}
+
