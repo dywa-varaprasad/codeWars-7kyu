faac0f2b202bb83621c0b23668c7f3738e83837f
DywaVaraprasad <105714346+dywa-varaprasad@users.noreply.github.com>
2022-08-10 10:20:36 +0530

    solved 1st 7kyu problem under 5mins
    let's go 🚀🚀

---
+++ AddTwoNumsOfAnyType.js
@@ -0,0 +1,21 @@
+
+/*
+be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+
+Note: a and b are not ordered!
+*/
+
+
+//my solution 
+function getSum( a,b )
+{
+  if(a=b){
+    return a;
+  }
+  
+  else if (-a ){ 
+    return a+b
+
+}
+
+}
